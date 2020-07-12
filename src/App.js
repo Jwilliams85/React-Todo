43baeb360f1e5e83cb2aa34cefaa16a3cb57d51c
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-07-12 04:12:40 -0400

    app changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Todo from './components/Todo'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+      name: "Ryan"
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className = "App">
+        <h1>Hello {this.state.name}</h1>
         <h2>Welcome to your Todo App!</h2>
+        <Todo/>
       </div>
     );
   }
